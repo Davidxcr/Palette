54a957b5a9d3924684b3387dbe823af21e61d077
Davidxcr <vodolatunji@gmail.com>
2022-02-08 11:42:38 -0600

    New Folder for palette app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Palette from './Palette';
+import seedColors from './seedColors';
+import { generatePalette } from './colorHelpers'
+
+class App extends Component {
+  render() {
+    console.log(generatePalette(seedColors[2]))
+    return (
+      <div className='App'>
+        <Palette palette={generatePalette(seedColors[4])} />
+      </div>
+    );
+  }
+}
+
+export default App;
